3a127272bf9306d48db36aa67fd56fd1de3a20fb
AizenSousuke <puteranikdimas@yahoo.com.sg>
2019-06-25 10:58:45 +0800

    Created function to edit hero properly

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -12,6 +12,8 @@ export class HeroFormComponent implements OnInit {
   powers = ['Really smart', 'Rich', 'Resourceful'];
   model;
   submitted = false;
+  editting = false;
+  idEditting : number;
   heroSaved = [];
 
   constructor(private _heroService : HeroServiceService) { }
@@ -23,8 +25,17 @@ export class HeroFormComponent implements OnInit {
   onSubmit() {
     
     console.log(this.model);
-    let heroSubmitted = {"id" : this.heroSaved.length + 1, "name" : this.model.name, "power" : this.model.power};
-    this.heroSaved.push(heroSubmitted);
+    let heroSubmitted;
+    if (!this.editting) {
+      heroSubmitted = {"id" : this.heroSaved.length + 1, "name" : this.model.name, "power" : this.model.power};
+      this.heroSaved.push(heroSubmitted);
+      console.log("Added new hero!");
+    } else {
+      //this.heroSaved[this.idEditting] = {"id" : this.idEditting,"name" : this.model.name, "power" : this.model.power};
+      this.heroSaved[this.idEditting - 1].name = this.model.name;
+      this.heroSaved[this.idEditting - 1].power = this.model.power;
+      console.log("Editted hero!");
+    }
 
     // Use a service to transfer the hero list out
     this._heroService.setHeroes(this.heroSaved);
@@ -40,10 +51,17 @@ export class HeroFormComponent implements OnInit {
   newHero() {
     this.model = new Hero(this.heroSaved.length + 1, "", "");
     this.submitted = false;
+    this.editting = false;
   }
 
-  onEdit() {
+  onEdit(_form) {
+    // Transfer the data to the form's field
+    //console.log(this.heroSaved[this.heroSaved.length - 1].power);  
+    this.idEditting = this.heroSaved.length;
+    console.log("Editing ID: " + this.idEditting);  
+    _form.form.setValue({ "name" : this.heroSaved[this.heroSaved.length - 1].name, "power" : this.heroSaved[this.heroSaved.length - 1].power });
     this.submitted = false;
+    this.editting = true;
   }
 
 }
